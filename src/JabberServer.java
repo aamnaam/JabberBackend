a7dad30ccd7bba545d11ac376b2d88ff03f9663e
Mohammed Aamaan <71080707+aamnaam@users.noreply.github.com>
2021-05-17 01:53:58 +0100

    Remove unused client variable

--- src/JabberServer.java
+++ src/JabberServer.java
@@ -1,14 +1,20 @@
+/*
+ *
+ * @author Mohd Aamaan, 2252554
+ * Main class for running the Jabber backend.
+ */
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class JabberServer implements Runnable {
     private static final int PORT_NUMBER = 44444;
-    private ServerSocket serverSocket;
+    private final ServerSocket serverSocket;
 
     public JabberServer() throws IOException {
         serverSocket = new ServerSocket(PORT_NUMBER);
-//        serverSocket.setSoTimeout(5000);
+//        serverSocket.setSoTimeout(300);
         new Thread(this).start();
     }
 
@@ -21,13 +27,12 @@ public class JabberServer implements Runnable {
         try {
             System.out.println("Server waiting for clients");
             while (true) {
-                Thread.sleep(1000);
+                Thread.sleep(100);
                 Socket clientSocket = serverSocket.accept();
-                ClientConnection client
-                        = new ClientConnection(clientSocket, new JabberDatabase());
+                new ClientConnection(clientSocket, new JabberDatabase());
             }
         } catch (Exception e) {
-                e.printStackTrace();
+            e.printStackTrace();
         }
     }
 }
